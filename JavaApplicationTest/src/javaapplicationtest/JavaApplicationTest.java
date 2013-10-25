c49a9cd1bc05e5ea59e32baaac07426cb1411a5b
sherinab <ab.sherin@gmail.com>
2013-10-25 15:25:36 -0400

    SOP STOP

--- JavaApplicationTest/src/javaapplicationtest/JavaApplicationTest.java
+++ JavaApplicationTest/src/javaapplicationtest/JavaApplicationTest.java
@@ -15,6 +15,10 @@ public class JavaApplicationTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
         System.out.println("hello world");
+        //change
+        
+      System.out.println("STOP");
     }
 }
